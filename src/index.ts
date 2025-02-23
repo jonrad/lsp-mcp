3e604a8ee928a4561d9cbd2aa68d556c359c9be9
Jon Radchenko <jonrad@gmail.com>
2025-02-23 18:27:34 -0500

    working through mcp

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,67 @@
 import { startLsp, initialize } from "./lsp";
+import { startMcp, createMcp } from "./mcp";
+import * as fs from "fs/promises";
+import { z } from "zod";
+import * as protocol from "vscode-languageserver-protocol";
+import path from "path";
+
+const URI_SCHEME = "lsp";
+function buildUri(...paths: string[]) {
+  return `${URI_SCHEME}://` + path.resolve(...paths);
+}
+
+
 async function main() {
   const lsp = await startLsp("sh", [
     "-c",
     "yarn --silent typescript-language-server --stdio --log-level 4 | tee lsp.log",
   ]);
-  
-  await initialize(lsp);
-  console.log("Initialized");
-  lsp.dispose();
+
+  const mcp = createMcp();
+  // Add an addition tool
+  mcp.tool(
+    protocol.DocumentSymbolRequest.method, // name
+    "Get the symbols in a file", // description
+    { file: z.string() }, // args
+    async ({ file }) => {
+      try {
+
+        const contents = await fs.readFile(file, "utf8");
+        const uri = buildUri(file);
+        const notification = await lsp.sendNotification(
+          protocol.DidOpenTextDocumentNotification.method,
+          {
+            textDocument: {
+              uri: uri,
+              languageId: "typescript",
+              version: 1,
+              text: contents,
+            },
+          },
+        );
+        const result = await lsp.sendRequest(
+          protocol.DocumentSymbolRequest.method,
+          { textDocument: { uri: uri } },
+        );
+
+        return {
+          content: [{ type: "text", text: JSON.stringify(result, null, 2) }],
+        };
+      } catch (error) {
+        console.error(`Error getting symbols: ${error}`);
+        throw error;
+      }
+    },
+  );
+
+  await startMcp(mcp);
+  console.log("Started MCP");
+
+  // await initialize(lsp);
+  // console.log("Initialized");
+  // lsp.dispose();
+  //
+  // lsp.dispose();
 }
 
 main();
--- src/mcp.ts
+++ src/mcp.ts
@@ -2,20 +2,17 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 
-// Create an MCP server
-const server = new McpServer({
-  name: "LSP",
-  version: "0.1.0",
-});
+export function createMcp() {
+  // Create an MCP server
+  return new McpServer({
+    name: "LSP",
+    version: "0.1.0",
+    description: "A language server protocol server",
+  });
+}
 
-// Add an addition tool
-server.tool("add", { a: z.number(), b: z.number() }, async ({ a, b }) => ({
-  content: [{ type: "text", text: String(a + b) }],
-}));
-
-// Start receiving messages on stdin and sending messages on stdout
-const transport = new StdioServerTransport();
-
-export async function mcpMain() {
-  await server.connect(transport);
+export async function startMcp(mcp: McpServer) {
+  // Start receiving messages on stdin and sending messages on stdout
+  const transport = new StdioServerTransport();
+  await mcp.connect(transport);
 }
